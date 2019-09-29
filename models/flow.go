68985d3a71586c68cbd747cb3ee295912ee06201
Odohi David <odohi.david@gmail.com>
2019-09-29 16:55:51 +0100

    add validation tags to the flow struct type

--- models/flow.go
+++ models/flow.go
@@ -1,10 +1,25 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Flow struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Name        string `json:"name" valid:"required"`
+	Description string `json:"description" valid:"required"`
 	Functions   []Function
+}
+
+func (flow Flow) Validate(db *gorm.DB) {
+	if len(flow.Name) < 0 {
+		// name is not available...
+		db.AddError(errors.New("Name cannot be blank"))
+	}
+	if len(flow.Description) < 0 {
+		// name is not available...
+		db.AddError(errors.New("Description cannot be blank"))
+	}
 }
