b43f66746f4fb5e133cc90b2592f1baa47913fda
Odohi David <odohi.david@gmail.com>
2019-09-29 18:59:02 +0100

    Add validation to function model

--- models/function.go
+++ models/function.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Function struct {
 	gorm.Model
@@ -8,4 +12,19 @@ type Function struct {
 	File   string `json:"file"`
 	FlowID uint   `json:"flow_id"`
 	Flow   Flow   `json:"flow"`
+}
+
+func (f Function) Validate(db *gorm.DB) {
+	if len(f.Name) < 0 {
+		// name is not available...
+		db.AddError(errors.New("Name cannot be blank"))
+	}
+	if len(f.File) < 0 {
+		// name is not available...
+		db.AddError(errors.New("File cannot be blank"))
+	}
+	if f.FlowID < 1 {
+		// name is not available...
+		db.AddError(errors.New("Flow ID cannot be blank"))
+	}
 }
