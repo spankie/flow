21084dc45e280ad3bbb7f7ebf408f32ea56758f5
Odohi David <odohi.david@gmail.com>
2019-09-29 16:55:00 +0100

    add validator plugin for gorm

--- db/db.go
+++ db/db.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/qor/validations"
 )
 
 var DB *gorm.DB
@@ -13,8 +14,10 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 	log.Println(os.Getenv("DB_URL"))
+
 	DB, err = gorm.Open("mysql", os.Getenv("DB_URL"))
 	if err != nil {
 		log.Fatal("Could not connect to database. == ", err)
 	}
+	validations.RegisterCallbacks(DB)
 }
