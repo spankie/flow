129be5112ae4385e2fadf707adf2769e81c52164
Odohi David <odohi.david@gmail.com>
2019-09-29 21:43:36 +0100

    add description to the function model

--- models/function.go
+++ models/function.go
@@ -8,23 +8,28 @@ import (
 
 type Function struct {
 	gorm.Model
-	Name   string `json:"name"`
-	File   string `json:"file"`
-	FlowID uint   `json:"flow_id"`
-	Flow   Flow   `json:"flow"`
+	Name        string `json:"name"`
+	File        string `json:"file"`
+	FlowID      uint   `json:"flow_id"`
+	Description string `json:"description"`
+	// Flow   Flow   `json:"flow" binding:"-" form:"-"`
 }
 
 func (f Function) Validate(db *gorm.DB) {
-	if len(f.Name) < 0 {
+	if len(f.Name) < 1 {
 		// name is not available...
 		db.AddError(errors.New("Name cannot be blank"))
 	}
-	if len(f.File) < 0 {
+	if len(f.File) < 1 {
 		// name is not available...
 		db.AddError(errors.New("File cannot be blank"))
 	}
 	if f.FlowID < 1 {
 		// name is not available...
 		db.AddError(errors.New("Flow ID cannot be blank"))
+	}
+	if len(f.Description) < 1 {
+		// name is not available...
+		db.AddError(errors.New("Description cannot be blank"))
 	}
 }
