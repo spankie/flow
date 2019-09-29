f3cddc8a90cd058327a20cbffee0aa8da94d1fac
Odohi David <odohi.david@gmail.com>
2019-09-29 15:15:32 +0100

    rename init function to connect db so that the env variables can be accessed

--- db/db.go
+++ db/db.go
@@ -10,10 +10,11 @@ import (
 
 var DB *gorm.DB
 
-func init() {
+func ConnectDB() {
 	var err error
+	log.Println(os.Getenv("DB_URL"))
 	DB, err = gorm.Open("mysql", os.Getenv("DB_URL"))
 	if err != nil {
-		log.Fatal("Could not connect to database.")
+		log.Fatal("Could not connect to database. == ", err)
 	}
 }
